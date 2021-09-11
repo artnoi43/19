c5760357aeb4e29fec988134e862b5abcce2f0d0
artnoi <artdrawin@gmail.com>
2021-09-11 20:09:12 +0700

    Updated Links

--- src/components/Links.js
+++ src/components/Links.js
@@ -6,19 +6,17 @@ import { appContext } from '../App';
 function Links() {
     const { path, setPath } = useContext(appContext);
     return (
-        <>
-            <ButtonGroup>
-                <Button onClick={() => setPath("/")} disabled={path === "/"}>
-                    <Link to="/">General Data</Link><br />
-                </Button>
-                <Button onClick={() => setPath("/vaccination")} disabled={path === "/vaccination"}>
-                    <Link to="/vaccination">Vaccinaion Data</Link><br />
-                </Button>
-                <Button onClick={() => setPath("/test")} disabled={path === "/test"}>
-                    <Link to="/test">Test Data</Link>
-                </Button>
-            </ButtonGroup>
-        </>
+        <ButtonGroup>
+            <Button onClick={() => setPath("/")} disabled={path === "/"}>
+                <Link to="/">General Data</Link><br />
+            </Button>
+            <Button onClick={() => setPath("/vaccination")} disabled={path === "/vaccination"}>
+                <Link to="/vaccination">Vaccinaion Data</Link><br />
+            </Button>
+            <Button onClick={() => setPath("/test")} disabled={path === "/test"}>
+                <Link to="/test">Test Data</Link>
+            </Button>
+        </ButtonGroup>
     );
 };
 
